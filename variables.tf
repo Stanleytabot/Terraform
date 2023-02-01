36bfac0018a8e9471f055f0574d0e37c8993a2c6
Stanleytabot <99697986+Stanleytabot@users.noreply.github.com>
2023-02-01 14:09:06 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
-
+variable "region" {}
+variable "instance_type" {}
+# variable "availability_zone" {}
+# variable "subnet_id" {}
+# variable "vpc_security_group_ids" {}
+variable "Devo" {}
+variable "ec2name" {}
