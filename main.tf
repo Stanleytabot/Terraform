13ecd9eaccfc94203082b9a141d0afb7dd0296d3
Stanleytabot <99697986+Stanleytabot@users.noreply.github.com>
2023-02-01 15:04:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "aws" {
+   region   = var.region
+   #profile = "terraform-user-new-acct"
+ }
 
+# Data sources to get VPC 
+#data "aws_vpc" "default" {
+ # default = true
+#}
+
+#create sg
+module "security_group" {
+  source = "./security-group-ssh"
+}
+
+#create ec2 instance
+module "ec2_instance" {
+  source                        = "./Ec2"
+  instance_type                 = var.instance_type
+  #key_name                      = var.key_name
+}
